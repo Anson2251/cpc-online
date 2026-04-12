534ccd93b2d3975a20c88f98558b9928148b9487
anson2251 <zhu_heyan@163.com>
2026-04-12 23:21:00 +0800

    fix(font): fix the ui font

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,3 @@ const themeOverrides = {
   </NConfigProvider>
 </template>
 
-<style>
-@font-face{font-family:"ZSFT-443";src:url("https://fontsapi.zeoseven.com/443/italic.woff2") format("woff2");font-style:italic;font-weight:100 900;font-display:swap;}@font-face{font-family:"ZSFT-443";src:url("https://fontsapi.zeoseven.com/443/main.woff2") format("woff2");font-style:normal;font-weight:100 900;font-display:swap;}
-@font-face{font-family:"ZSFT-go";src:url("https://fontsapi.zeoseven.com/go/italic.woff2") format("woff2");font-style:italic;font-weight:100 900;font-display:swap;}@font-face{font-family:"ZSFT-go";src:url("https://fontsapi.zeoseven.com/go/main.woff2") format("woff2");font-style:normal;font-weight:100 900;font-display:swap;}
-</style>
